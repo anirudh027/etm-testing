fcac7ad0ab6a54ebe74a75d33bea83d02fa4d996
anirudh <anirudhsai027@gmail.com>
2024-06-19 13:08:09 +0200

    changes

---
+++ ProM610.bat
@@ -0,0 +1,24 @@
+@GOTO start
+
+:add
+ @set X=%X%;%1
+ @GOTO :eof
+
+:start
+@set X=.\dist\ProM-Framework-6.10.110.jar
+@set X=%X%;.\dist\ProM-Contexts-6.10.62.jar
+@set X=%X%;.\dist\ProM-Models-6.10.40.jar
+@set X=%X%;.\dist\ProM-Plugins-6.9.70.jar
+
+@for /R .\lib %%I IN ("*.jar") DO @call :add .\lib\%%~nI.jar
+
+@java^
+ -Xmx4G^
+ -da^
+ -classpath "%X%"^
+ -Djava.library.path=.//lib^
+ -Djava.system.class.loader=org.processmining.framework.util.ProMClassLoader^
+ -Djava.util.Arrays.useLegacyMergeSort=true^
+ org.processmining.contexts.uitopia.UI
+  
+set X=
